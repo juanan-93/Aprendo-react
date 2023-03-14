75a41ad3de7a47f62914798a8ea4f3230c9867b0
Your Name <you@example.com>
2023-03-14 12:28:29 +0100

    react

---
+++ teo-aprende-react/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { useState} from 'react';
+import Table from './componentes/table/Table';
+
+function App() {
+
+
+
+
+
+  return (
+    <div className="App">
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
---
+++ teo-aprende-react/src/componentes/table/Table.jsx
@@ -0,0 +1,14 @@
+import React, { useState} from 'react';
+
+function Table() {
+
+    return (
+        <>  
+        
+        </>
+            
+    );
+
+}
+
+export default Table;
